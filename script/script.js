303c886df613e33f44ca145d725c8d1449d8bdbb
AsSamha <assamhaa@gmail.com>
2018-01-07 11:44:54 +0200

    * Added ground class with and implementing it * Updated image/2.png

--- script/script.js
+++ script/script.js
@@ -1,38 +1,57 @@
-var MovingObject = function () {
+var MovingObject = function() {
 	// the image that appears as the object
 	this.image = new Image();
 	this.image.src = 'images/1.png';
+	this.imageXDimension = 100;
+	this.imageYDimension = 100;
 	// coordintates of the object where the origin is at the top left of the canvas
-	this.Xpos = -100;
+	this.Xpos = 800;
 	this.Ypos = 100;
 	// speed components of the object in the horizontal and vertical directions respectively
-	this.Xspeed = 3;
+	this.Xspeed = -3;
 	this.Yspeed = 0;
-
-	this.visible = true;	// flag to remove the object when the object outs the boundaries
 }
 MovingObject.prototype.move = function() {
-	if(this.Xpos < -100 || this.Ypos < -100 || this.Xpos > 800 || this.Ypos > 600) { // check when the ..
-		this.visible = false; // .. object gets out the boundaries of the canvas
-		this.Xpos = -100;
+	if(this.Xpos < -this.imageXDimension || this.Ypos < -this.imageYDimension || this.Xpos > 700 + this.imageXDimension || this.Ypos > 500 + this.imageYDimension) { // check when the ..
+		this.Xpos = 700 + this.imageXDimension;
 		this.Ypos = 100;
-	} else {
-		this.visible = true;
 	}
 	ctx.drawImage(this.image, this.Xpos += this.Xspeed, this.Ypos += this.Yspeed);
 }
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var Ground = function() {
+	MovingObject.call(this);
+	this.image.src = 'images/2.png';
+	this.imageXDimension = 2400;
+	this.imageYDimension = 24;
+	this.Xspeed = -4;
+	this.Xpos = 0, this.Ypos = 450;
+	this.Xpos2 = this.imageXDimension, this.Ypos2 = 450;
+}
+Ground.prototype.move = function() {
+	if(this.Xpos < -this.imageXDimension) { // check when the ..
+		this.Xpos = this.imageXDimension;
+	} else if(this.Xpos2 < -this.imageXDimension) {
+		this.Xpos2 = this.imageXDimension;
+	}
+	ctx.drawImage(this.image, this.Xpos += this.Xspeed, this.Ypos);
+	ctx.drawImage(this.image, this.Xpos2 += this.Xspeed, this.Ypos);
+}
+
 var ctx = document.getElementById('canvas').getContext('2d');
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 var obj1 = new MovingObject;
+var obj2 = new MovingObject;
+var groundObj = new Ground;
+obj2.Xpos = 600;
 function init() {
 	window.requestAnimationFrame(draw);
 }
 function draw() {
 	ctx.clearRect(0, 0, 700, 500);
-	if(obj1.visible)
-		obj1.move();
+	obj1.move();
+	obj2.move();
+	groundObj.move();
 	window.requestAnimationFrame(draw);
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
