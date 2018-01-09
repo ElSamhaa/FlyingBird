af0df66a1ee890e45782ab32f9170ce9365b6adf
AsSamha <assamhaa@gmail.com>
2018-01-09 02:05:16 +0200

    * Separated the functionality of - previously - move function into update() and draw() functions for each class * Added the enhancement of implementing the rAF function as a timed-rAF to improve smoothness and performance * Minor changes

--- script/script.js
+++ script/script.js
@@ -11,16 +11,25 @@ var MovingObject = function() {
 	this.Xspeed = -2;
 	this.Yspeed = 0;
 }
-MovingObject.prototype.move = function() {
-	// reset position when the whole object gets out of the canvas borders
-	if(this.Xpos < -this.imageXDimension || this.Ypos < -this.imageYDimension
-	|| this.Xpos > 700 + this.imageXDimension || this.Ypos > 500 + this.imageYDimension) {
+
+MovingObject.prototype.update = function() { // function to update the coordinates of the object
+	this.Xpos += this.Xspeed, this.Ypos += this.Yspeed;
+	// resets position when the whole object gets out of the canvas borders
+	if(this.Xpos < -this.imageXDimension		||
+	   this.Ypos < -this.imageYDimension		||
+	   this.Xpos > 700 + this.imageXDimension 	||
+	   this.Ypos > 500 + this.imageYDimension)	{
+
 		this.Xpos = 700 + this.imageXDimension;
 		this.Ypos = 100;
 	}
-	ctx.drawImage(this.image, this.Xpos += this.Xspeed, this.Ypos += this.Yspeed); // draw into canvas
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+MovingObject.prototype.draw = function() { // draw into canvas
+	ctx.drawImage(this.image, this.Xpos, this.Ypos);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 var Cloud = function() {
 	MovingObject.call(this);
 	this.image.src = 'images/3.png';
@@ -29,72 +38,117 @@ var Cloud = function() {
 	this.Xspeed = -0.5;
 	this.Xpos = 700, this.Ypos = 50;
 }
-Cloud.prototype.move = function() {
+
+Cloud.prototype.update = function() {
+	this.Xpos += this.Xspeed;
+
 	if(this.Xpos < -this.imageXDimension) { // checks when the image totally outs the canvas borders
 		this.Xpos = 700; // sets the Xposition to the right border of the canvas
 	}
-	ctx.drawImage(this.image, this.Xpos += this.Xspeed, this.Ypos);
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Cloud.prototype.draw = function() {
+	ctx.drawImage(this.image, this.Xpos, this.Ypos);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 var Obstacle = function() {
 	MovingObject.call(this);
 }
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 var Cactus = function() {
 	Obstacle.call(this);
+
 	this.image.src = 'images/4.png';
 	this.imageXDimension = 49;
 	this.imageYDimension = 102;
+
+	this.Xpos = 700, this.Ypos = 368;
+
 	this.Xspeed = -1;
-	this.Xpos = 700, this.Ypos = 366;
 }
-Cactus.prototype.move = function() {
+
+Cactus.prototype.update = function() {
+	this.Xpos += this.Xspeed;
+
 	if(this.Xpos < -this.imageXDimension) { // checks when the image totally outs the canvas borders
 		this.Xpos = 700; // sets the Xposition to the right border of the canvas
 	}
-	ctx.drawImage(this.image, this.Xpos += this.Xspeed, this.Ypos);
 }
+
+Cactus.prototype.draw = function() {
+	ctx.drawImage(this.image, this.Xpos, this.Ypos);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 var Ground = function() {
 	Obstacle.call(this);
+
 	this.image.src = 'images/2.png';
 	this.imageXDimension = 2400;
 	this.imageYDimension = 24;
-	this.Xspeed = -1;
-	this.Xpos = 0, this.Ypos = 450;
-	this.Xpos2 = this.imageXDimension, this.Ypos2 = 450;
-}
-Ground.prototype.move = function() {
-	if(this.Xpos < -this.imageXDimension) { // checks when the image totally outs the canvas borders
-		this.Xpos = this.imageXDimension; // 
-	} else if(this.Xpos2 < -this.imageXDimension) {
-		this.Xpos2 = this.imageXDimension;
-	}
-	ctx.drawImage(this.image, this.Xpos += this.Xspeed, this.Ypos);
-	ctx.drawImage(this.image, this.Xpos2 += this.Xspeed, this.Ypos);
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+	this.Xpos = 0,						this.Ypos = 450;
+	this.Xpos2 = this.imageXDimension,	this.Ypos2 = 450; // two images drawn successively after each other
+
+	this.Xspeed = -1;
+}
+
+Ground.prototype.update = function() {
+	this.Xpos2 += this.Xspeed;
+	this.Xpos += this.Xspeed;
+
+	if(this.Xpos < -this.imageXDimension) { // checks when the first image totally outs the canvas borders
+		this.Xpos = this.imageXDimension; // resets the Xpos of the first image
+	} else if(this.Xpos2 < -this.imageXDimension) { // when the second image outs the canvas borders
+		this.Xpos2 = this.imageXDimension; // resets the Xpos for the second image
+	}
+}
+
+Ground.prototype.draw = function() {
+	ctx.drawImage(this.image, this.Xpos, this.Ypos);
+	ctx.drawImage(this.image, this.Xpos2, this.Ypos);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 var ctx = document.getElementById('canvas').getContext('2d');
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-var obj1 = new MovingObject;
-var obj2 = new MovingObject;
-var groundObj = new Ground;
-var cloudObj = new Cloud;
-var cactusObj = new Cactus;
-obj2.Xpos = 600;
-function init() {
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+var then = 0, deltaTimeFrame = 0;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+var obj1 = new MovingObject; // a bird demo
+var obj2 = new MovingObject; // 2nd bird demo
+obj2.Xpos = 600; // starting position for second bird
+var groundObj = new Ground; // a ground demo
+var cloudObj = new Cloud; // a cloud demo
+var cactusObj = new Cactus; // a cactus demo
+
+function draw(tframe) { // tframe is an implicit parameter automatically passed to the callback function ..
+	// i.e. draw function, by the requestAnimationFrame(). tframe represents a timestamp in milliseconds ..
+	// starting the moment the user load the page
+	deltaTimeFrame = tframe - then;
+	if(deltaTimeFrame >= 30) { // to set the refresh rate of the canvas as once every 30 milliseconds
+		ctx.clearRect(0, 0, 700, 500); // to clear the canvas each frame
+		cloudObj.update();
+		obj1.update();
+		obj2.update();
+		groundObj.update();
+		cactusObj.update();
+		cloudObj.draw();
+		obj1.draw();
+		obj2.draw();
+		groundObj.draw();
+		cactusObj.draw();
+		then = tframe;
+	}
+
+	window.requestAnimationFrame(draw); // a non-blocking recursive call for the requestAnimationFrame()
+}
+
+function init() { // function that initialize the game environment
 	window.requestAnimationFrame(draw);
 }
-function draw() {
-	ctx.clearRect(0, 0, 700, 500); // to clear the canvas each frame
-	cloudObj.move();
-	obj1.move();
-	obj2.move();
-	groundObj.move();
-	cactusObj.move();
-	window.requestAnimationFrame(draw);
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 init();
 
 
