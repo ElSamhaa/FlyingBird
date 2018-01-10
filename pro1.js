6e92683216eeaf9db395847333c9a9e80813af90
mtaweela <mohamed.taweela56@gmail.com>
2018-01-10 23:59:18 +0200

    transfering the crash functionality outside the bird (moving it into the game loop)

--- pro1.js
+++ pro1.js
@@ -55,10 +55,11 @@ MovingObject.prototype.setposx = function () {
 MovingObject.prototype.move = function () {
 };
 //##########################     bird class     #####
- var bird = function(x = 150, y = 100) {
+ var bird = function(x = 150, y = 100 , lives = 3) {
    MovingObject.call(this,x , y , dx = 0 , dy = 7);
    this.player_name;
-   this.lives = 3;
+   this.initiallives = lives;
+   this.lives = this.initiallives;
    this.scoree = 0;
  }
 
@@ -68,21 +69,20 @@ bird.prototype.constructor = bird;
 // ... dealing with bird movement
 bird.prototype.move = function( time = 90){
   window.addEventListener('keyup', function(event) {
-    if (event.keyCode === 32 && this.pos_y > 10 && keepjump === 'true') {
+    if (this.pos_y > 10 && keepjump === 'true') {
       this.pos_y -= 40;
       img.style.top = this.pos_y +'px';
     }
   }.bind(this))
 
+  // gravity effect
   grav = setInterval(function () {
     this.pos_y += this.dy;
     img.style.top= this.pos_y +"px";
     displaylives.src = birdwings[0];
     displaylives1.innerHTML = ` x${this.lives}`;
-    this.crash();
-
   }.bind(this), time);
-}
+};
 
 bird.prototype.wingmove = function () {
     wingmov = setInterval(function () {
@@ -91,43 +91,6 @@ bird.prototype.wingmove = function () {
        count = 0;
      }
     },90);
-};
-
-// ... dealing with bird crashing
-bird.prototype.crash = function () {
- if ( ( parseInt(getComputedStyle(obsimage).left) <= this.pos_x && this.pos_y+50 >= parseInt(getComputedStyle(obsimage).top) )
-      || this.pos_y > window.innerHeight-120 && this.lives > 0) {
-     this.pos_y = 100;
-     this.lives--;
-     displaylives1.innerHTML = ` x${this.lives}`;
-     this.whencrash();
- }
-};
-
-bird.prototype.whencrash = function () {
-  function clearintervals(){
-    clearInterval(grav);
-    clearInterval(wingmov);
-    clearInterval(scoreint);
-  }
-
-  clearintervals();
-  img.src = "images/5.png"
-
-  setTimeout(function() {
-    img.src = birdwings;
-    this.wingmove();
-    this.move();
-    this.printscore();
-  }.bind(this), 1000)
-
-  if (this.lives === 0) {
-   clearintervals();
-   startpage.style.display = 'none';
-   gamepage.style.display = 'none';
-   endpage.style.display = 'block';
-   showscore.innerHTML = `${this.player_name} Your Final Score Is: ${this.scoree}`
-  }
 };
 
 // ... dealing with player attributes
@@ -168,10 +131,11 @@ bird.prototype.main = function (){
   this.printscore();
 }
 
-//##########################    the obstacles class and proto types     ########
+//##########################    Obstacles Class  ########
 obsimage.src = imagesarray[Math.floor(Math.random() * imagesarray.length)];
 obsimage.height = heightarray[Math.floor(Math.random() * heightarray.length)];
 
+// append images instead of dealing with one obstacle
 var obstacles = function() {
   MovingObject.call(this, 10,0);
 }
@@ -179,22 +143,21 @@ var obstacles = function() {
 obstacles.prototype = Object.create(MovingObject.prototype);
 obstacles.prototype.constructor = obstacles;
 
-
-obstacles.prototype.setposx = function () {
-  this.pos_x += 10;
-  obsimage.style.right = this.pos_x +"px";
-  if (this.pos_x > window.innerWidth-140) {
-    this.pos_x = 10;
-    setTimeout(function() {
-      obsimage.src = imagesarray[Math.floor(Math.random() * imagesarray.length)];
-      obsimage.height = heightarray[Math.floor(Math.random() * heightarray.length)];
-    },100)
-  }
+obstacles.prototype.move = function () {
+  moveleft = setInterval( function(){
+      this.pos_x += 10;
+      obsimage.style.right = this.pos_x +"px";
+      if (this.pos_x > window.innerWidth) {
+        this.pos_x = 10;
+        obsimage.src = imagesarray[Math.floor(Math.random() * imagesarray.length)];
+        obsimage.height = heightarray[Math.floor(Math.random() * heightarray.length)];
+      }
+    }.bind(this), 50);
 };
 
-obstacles.prototype.obsmove = function () {
-  moveleft = setInterval(this.setposx.bind(this), 50);
-};
+  // var obstacle = document.createElement("img")		--> Creating The Element(get The Element)
+  // var gamediv = document.getElementById('myDiv')
+	// gamediv.appendChild(obstacle)
 
 //##########################     Cloud class     #####
 var Cloud = function() {
@@ -218,7 +181,7 @@ Background .prototype.constructor = obstacles;
 function gameloop() {
   var b1 = new bird;
   var obs = new obstacles;
-  obs.obsmove();
+  obs.move();
 
   b1.setplayername(playername.value);
   var printname = b1.getplayername();
@@ -226,10 +189,49 @@ function gameloop() {
 
   b1.main();
 
+  // ... dealing with bird crashing
+  setInterval(function crash () {
+  // solution for crashing .. mmove function get array of obstacles and check on them
+   if ( ( parseInt(getComputedStyle(obsimage).left) <= b1.pos_x && b1.pos_y+50 >= parseInt(getComputedStyle(obsimage).top) )
+        || b1.pos_y > window.innerHeight-120 && b1.lives > 0) {
+       b1.pos_y = 100;
+       b1.lives--;
+       displaylives1.innerHTML = ` x${b1.lives}`;
+       whencrash();
+   }
+  }, 50);
+
+
+  whencrash = function () {
+    function clearintervals(){
+      clearInterval(grav);
+      clearInterval(wingmov);
+      clearInterval(scoreint);
+    }
+
+    clearintervals();
+    img.src = "images/5.png"
+
+    setTimeout(function() {
+      img.src = birdwings;
+      b1.wingmove();
+      b1.move();
+      b1.printscore();
+    }, 1000)
+
+    if (this.lives === 0) {
+     clearintervals();
+     startpage.style.display = 'none';
+     gamepage.style.display = 'none';
+     endpage.style.display = 'block';
+     showscore.innerHTML = `${b1.player_name} Your Final Score Is: ${b1.scoree}`
+    }
+  };
+
   playagain.addEventListener("click", function() {
     b1.scoree = 0;
     b1.pos_y = 100;
-    b1.lives = 3;
+    b1.lives = b1.initiallives;
     b1.main();
     startpage.style.display = 'none';
     gamepage.style.display = 'block';
