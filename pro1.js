8c41980ded6137307beb273f114a6f9e017aaae7
mtaweela <mohamed.taweela56@gmail.com>
2018-01-13 00:57:49 +0200

    commit to save the last changes .. but still have fatal bugs

--- pro1.js
+++ pro1.js
@@ -1,43 +1,28 @@
-// last added variables
-var heightarray = ["70","90","110","130","150","170","190","210,230,300"];
-var header = document.getElementById('header-bar')
-var level = document.getElementById("level")
-var choosebtn = document.getElementById("choose")
-var levelsimg = document.getElementById("levels")
-var showlevel = document.getElementById("showlevel")
-var levels = [
-  document.getElementById("choose1"),
-  document.getElementById("choose2"),
-  document.getElementById("choose3")
-]
-var levelsPics = [ "url('images/12.png')" , "url('images/2.jpg')" , "url('images/3.jpg')" ]
-
 // ##### the global variables #####
 // pages
 var startpage = document.getElementById('startpage');
 var gamepage = document.getElementById('gamepage');
 var endpage = document.getElementById('endpage');
-// player attributes
+// player
 var playername = document.getElementById('playername1');
 var displayname = document.getElementById('displayname');
 var displaylives1 = document.getElementById('displaylives1');
 var displaylives = document.getElementById('displaylives');
 var score = document.getElementById('score');
 var showscore = document.getElementById('showscore');
-// first page attributes
-var playbutton = document.getElementById('startplay');
+// first page
 var character = document.getElementsByClassName('character');
-// game page attributes
+// game page
 var img = document.getElementById('bird1');
 img.width = 80;
 img.height = 60;
-// end page attributes
+// end page
 var endgame = document.getElementById('endgame');
 var playagainbtn = document.getElementById('playagain');
 // obstacle images
 var obsimage = document.getElementById('obsimage');
 var imagesarray = ["images/obs2.png","images/obs1.png"];
-var heightarray = ["150","200","250","300","350","400","500","550"];
+var heightarray = ["70","90","110","130","150","170","190","210,230,300"];
 // bird attributes
 var birdwings = [];
 var count = 0;
@@ -48,7 +33,19 @@ var birdImages = [
   ["images/11.png","images/22.png","images/33.png","images/44.png"],
   ["images/111.png","images/222.png","images/333.png","images/444.png"] // default images
 ]
-
+// levels
+var levelsPics = [ "url('images/12.png')" , "url('images/2.jpg')" , "url('images/3.jpg')" ]
+var level = document.getElementById("level")
+var choosebtn = document.getElementById("choose")
+var levelsimg = document.getElementById("levels")
+var showlevel = document.getElementById("showlevel")
+var levels = [
+  document.getElementById("choose1"),
+  document.getElementById("choose2"),
+  document.getElementById("choose3")
+]
+// header bar
+var header = document.getElementById('header-bar')
 //##########################     Moving Object Class     #####
 var MovingObject = function(x , y , dx , dy) {
   this.pos_x = x;
@@ -124,7 +121,7 @@ bird.prototype.wingmove = function () {
  bird.prototype.getscore = function () {
    return this.scoree;
  };
-
+// ... need to be moved into the environment class
  bird.prototype.printscore = function () {
    score.innerHTML = `Score: ${this.scoree}`;
    scoreint = setInterval(this.setscore.bind(this),3000);
@@ -137,9 +134,13 @@ bird.prototype.main = function (){
 }
 
 //##########################    Obstacles Class  ########
+// var obsimage = document.createElement("img");
+// var myDiv = document.getElementById('obsimage');
+// myDiv.appendChild(image);
+
 obsimage.src = imagesarray[Math.floor(Math.random() * imagesarray.length)];
 obsimage.height = heightarray[Math.floor(Math.random() * heightarray.length)];
-
+// obsimage.width = 50;
 // append images instead of dealing with one obstacle
 var obstacle = function(x = -50, y = 0, dx = 5) {
   MovingObject.call(this, x, y, dx );
@@ -149,7 +150,7 @@ obstacle.prototype = Object.create(MovingObject.prototype);
 obstacle.prototype.constructor = obstacle;
 
 obstacle.prototype.move = function () {
-  moveleft = setInterval( function(){
+  setInterval( function(){
       this.pos_x += this.dx;
       obsimage.style.right = this.pos_x +"px";
 
@@ -189,94 +190,69 @@ var characterChoose = function() {
   }
 }
 
-//##########################     levels Start    #####
-
-var levelsinit = function(){
-  showlevel.addEventListener("click", function() {
-      startpage.style.display = 'none';
-      level.style.display = 'block';
-
-      for (var i = 0; i < levelsPics.length; i++) {
-        l = levelsPics[i];
-        levels[i].addEventListener("click" , function (e , levelsPic = levelsPics) {
-            gamepage.style.backgroundImage = levelsPic;
-            characterChoose();
-            level.style.display = 'none';
-            gamepage.style.display = 'block';
-            gameloop();
-        });
-      }
-  })
-
-}
-
 //##########################    the game loop     ############
-// game loope should be object that get the level and the other parameters
-
 gameloop = function (level = 1) {
-  // create the main player
+  this.level = level;
+  // create the main player , wingmove
   this.birdplayer = new bird;
-  console.log(this.birdplayer)
-
-  // create the obstacles
-  groundObstacles = [];
-  for (var i = 0; i < level * 3; i++) {
-    groundObstacles[i] = new obstacle(10,0,level*10);
-    groundObstacles[i].move();
-  }
-
-  const Env = new Environment()
-  Env.move();
-
   this.birdplayer.setplayername(playername.value);
   var printname = this.birdplayer.getplayername();
   displayname.innerHTML = `Player: ${printname}`;
 
-  this.birdplayer.main();
+  const Env = new Environment();
+  Env.move();
+};
 
-  // ... dealing with bird crashing
-  setInterval(function crash () {
-  // solution for crashing .. mmove function get array of obstacles and check on them
+gameloop.prototype.createMoveObstacles = function (){
+  obs = new obstacle(10 , 0 , this.level*10)
+  // this.groundObstacles = [];
+  // for (var i = 0; i < this.level * 3; i++) {
+  //   this.groundObstacles[i] = new obstacle(10 , 0 , this.level*10);
   //
+  //   setTimeout(this.groundObstacles[i].move(),300)
+  //   // console.log(this.groundObstacles[i])
+  // }
+}
+
+gameloop.prototype.crashDetection = function(){
+  setInterval(function () {
+  // solution for crashing .. mmove function get array of obstacles and check on them
   if ( ( (parseInt(getComputedStyle(obsimage).left) <= this.birdplayer.pos_x) && (this.birdplayer.pos_y+50 >= parseInt(getComputedStyle(obsimage).top)) )
       || this.birdplayer.pos_y > 500 && this.birdplayer.lives > 0) {
         this.birdplayer.pos_y = 20;
         this.birdplayer.lives--;
         displaylives1.innerHTML = ` x${this.birdplayer.lives}`;
-        whencrash();
+        this.whencrash();
      }
   }.bind(this), 50);
+}
 
-
-  whencrash = function () {
-
-    function clearintervals(){
-      clearInterval(grav);
-      clearInterval(wingmov);
-      clearInterval(scoreint);
-    }
-
-    clearintervals();
-    img.src = "images/5.png"
-
-    setTimeout(function() {
-      img.src = birdwings[0];
-      this.birdplayer.wingmove();
-      this.birdplayer.move();
-      this.birdplayer.printscore();
-    }.bind(this), 1000)
-
-
-    if (this.birdplayer.lives === 0) {
-     clearintervals();
-     startpage.style.display = 'none';
-     gamepage.style.display = 'none';
-     endpage.style.display = 'block';
-
-     showscore.innerHTML = `${this.birdplayer.player_name} Your Final Score Is: ${this.birdplayer.scoree}`
-   }
+gameloop.prototype.whencrash = function () {
+  function clearintervals(){
+    clearInterval(grav);
+    clearInterval(wingmov);
+    clearInterval(scoreint);
   }
 
+  clearintervals();
+  img.src = "images/5.png"
+
+  setTimeout(function() {
+    img.src = birdwings[0];
+    this.birdplayer.main();
+  }.bind(this), 1000)
+
+  if (this.birdplayer.lives === 0) {
+   clearintervals();
+   startpage.style.display = 'none';
+   gamepage.style.display = 'none';
+   endpage.style.display = 'block';
+   endgame.src = "images/Replay.png";
+   showscore.innerHTML = `${this.birdplayer.player_name} Your Final Score Is: ${this.birdplayer.scoree}`
+ }
+}
+
+gameloop.prototype.Playagain = function(){
   playagain.addEventListener("click", function() {
     this.birdplayer.scoree = 0;
     this.birdplayer.pos_y = 20;
@@ -287,25 +263,21 @@ gameloop = function (level = 1) {
     header.style.display = 'block';
     endpage.style.display = 'none';
   }.bind(this))
-};
+}
 
-
-//##########################     starting the game normally without levels    #####
-// play button listner
-playbutton.addEventListener("click", function() {
-  characterChoose();
-  startpage.style.display = 'none';
-  gamepage.style.display = 'block';
-  header.style.display = 'block';
-  newgame = gameloop(); // ... should take the level
-})
-
+gameloop.prototype.main = function(){
+  this.birdplayer.main();
+  this.createMoveObstacles();
+  this.crashDetection();
+  this.Playagain();
+}
 
 //##########################     loading page     #####
 function loadingpage() {
   var bar = document.getElementById('progress-bar')
   var prog = document.getElementById('progress');
   var progress = 25;
+
   loadinginterval = setInterval(function() {
     if (progress === 500) {
       clearInterval(loadinginterval);
@@ -319,9 +291,53 @@ function loadingpage() {
   },10)
 }
 
+//##########################     starting the game normally without levels    #####
+playbutton = function (){
+  var playbutton = document.getElementById('startplay');
+  playbutton.addEventListener("click", function() {
+    characterChoose();
+    startpage.style.display = 'none';
+    gamepage.style.display = 'block';
+    header.style.display = 'block';
+    var newgame = new gameloop(); // ... should take the level
+    newgame.main();
+  })
+}
 
+//##########################     Start Levels    #####
+var levelsinit = function(){
+  showlevel.addEventListener("click", function() {
+      startpage.style.display = 'none';
+      level.style.display = 'block';
+      // for (var i = 0; i < 3; i++) {
+      startlevel = function(){
+        characterChoose();
+        level.style.display = 'none';
+        gamepage.style.display = 'block';
+        var newgame = new gameloop();
+        newgame.main();
+      }
+        levels[0].addEventListener("click" , function () {
+            gamepage.style.backgroundImage = levelsPics[0];
+            startlevel()
+        });
+
+        levels[1].addEventListener("click" , function () {
+            gamepage.style.backgroundImage = levelsPics[1];
+            startlevel()
+        });
+        levels[2].addEventListener("click" , function () {
+            gamepage.style.backgroundImage = levelsPics[2];
+            startlevel()
+        });
+      // }
+  })
+}
+
+//##########################     initiate the game     #####
 function initiateGame(){
   loadingpage();
+  playbutton();
   levelsinit();
 }
 
